44bdf7f88849259879d77a024c825134cc31f50f
Hitesh Soni <sonihitesh2024@gmail.com>
2025-01-14 11:29:41 +0530

    tuesday

--- 04_ifStatements.py
+++ 04_ifStatements.py
@@ -52,4 +52,7 @@ num2=int(input("enter second num: "))
 num3=int(input("enter third num: "))
 if(num1>=num2 and num1>=num3):
     print("num1 is greatest")
-elif
+elif(num2>=num1 and num2>=num3):
+    print("num2 is greatest")
+elif(num3>=num1 and num3>=num2):
+    print("num3 is greatest")
